2d14f590a03525f3106a9ec98d55ee6bdab44378
bernayasarr <103641575+bernayasarr@users.noreply.github.com>
2022-05-29 16:26:03 +0300

    Layout Project
    This is my first project in Kotlin. My project has 3 screens.

---
+++ PremiumActivity.kt
@@ -0,0 +1,11 @@
+package com.bernayasar.myproject
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class PremiumActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_premium)
+    }
+}
